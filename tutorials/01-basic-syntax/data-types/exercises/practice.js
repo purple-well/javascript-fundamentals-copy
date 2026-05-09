48d3c6c61df69f3483a4aa0d489c0bd95335147d
Chegg <your.email@example.com>
2026-05-09 16:46:29 -0500

    Updated Changes to Cloned Repository - More to Come

--- tutorials/01-basic-syntax/data-types/exercises/practice.js
+++ tutorials/01-basic-syntax/data-types/exercises/practice.js
@@ -7,22 +7,32 @@
 //   D) log its type with the 'typeof' keyword.
 
 // 1. The name of your favorite character from Star Wars (any movie or show)
-
+let answerOne = "Call from Alien: Resurrection";
 // 2. The number of times you've watched the original Star Wars Trilogy
-
+let answerTwo = 2;
 // 3. Whether or not you even like Star Wars all that much
-
+let answerThree = true;
 // 4. How many total hours of Star Wars canon a person can watch (Google it!)
-
+let answerFour = 260;
 // 5. Create a variable that will eventually hold the true identity of Luke's father
 // (but let's say he doesn't know yet)
-
+let answerFive;
 // 6. A common or memorable quote from the original trilogy
-
+let answerSix = 'Greedo once said; "Koona t\'chuta, Solo?"';
 // 7. Whether Alderaan still exists or not (...too soon?)
-
+let answerSeven = false;
 // Identifying which data type to use in various situations and how
 // their syntax differs is an important foundation for learning
 // to code. This is the way!
+
+console.log(`
+${answerOne}
+${answerTwo}
+${answerThree}
+${answerFour}
+${answerFive}
+${answerSix}
+${answerSeven}
+`)
 
 // TODO: Before you go... don't forget to make a git commit!
