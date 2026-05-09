722b88c42c071402c359df1a0095569046889294
Chegg <your.email@example.com>
2026-05-09 16:28:28 -0500

    Completed Data Type Demo Worksheet Juan Moreno

--- tutorials/01-basic-syntax/data-types/exercises/demo.js
+++ tutorials/01-basic-syntax/data-types/exercises/demo.js
@@ -3,18 +3,31 @@
 // DEMO: Create a few examples of different data types.
 
 // String
-
+let favoriteCharacter = "Call from Alien: Resurrection";
 // Number
-
+let num = 900000;
 // Boolean
-
+let isChocolate = false;
 // Null
-
+let temporaryValue = null;
 // Undefined
-
+let score;
 // DEMO: Log each variable to the console.
-
+console.log (`
+${favoriteCharacter}
+${num}
+${isChocolate}
+${temporaryValue}
+${score}
+`)
 // DEMO: Log the type of each variable above to the console.
+console.log (`
+This is an example of a string: ${favoriteCharacter}
+This is an example of a number: ${num}
+This is an example of a boolean: ${isChocolate}
+This is an example of null: ${temporaryValue}
+This is an example of undefined: ${score}
+`)
 
 // DEMO: Make a git commit!
 
