e7df487b9089f42d825b27c1887eff2cd7bf8a35
Chegg <your.email@example.com>
2026-05-21 17:08:31 -0500

    input output practice juan m

--- tutorials/07-functions/input-output/exercises/practice.js
+++ tutorials/07-functions/input-output/exercises/practice.js
@@ -13,6 +13,12 @@
     Inside, log: "Warning: High radiation levels detected in Sector " followed by the parameter.
     Invoke the function passing "G" as the argument.
 */
+function reportSectorStatus(sectorLetter) {
+	console.log(`Warning: High radiation levels detected in Sector ${sectorLetter}`);
+}
+
+reportSectorStatus("G");
+
 
 /** EXERCISE 2: SHIELD POWER ESTIMATOR **/
 
@@ -22,6 +28,10 @@
     Inside, multiply 'basePower' by 'boostFactor' and RETURN the result.
     Do not call the function yet.
 */
+function calculateShieldPower(basePower, boostFactor) {
+	return basePower * boostFactor;
+}
+
 
 /** EXERCISE 3: CAPTURING SHIELD OUTPUT **/
 
@@ -31,6 +41,9 @@
     Capture the returned value in a constant named 'TOTAL_SHIELD'.
     Log 'TOTAL_SHIELD' to the console.
 */
+const TOTAL_SHIELD = calculateShieldPower(50, 3);
+console.log(`Total Shield Strength: ${TOTAL_SHIELD}`);
+
 
 /** EXERCISE 4: THE IS-EMPTY CHECKER **/
 
@@ -40,3 +53,13 @@
     If 'fuelLevel' is 0, return true. Otherwise, return false.
     Test the output of your function with different values using console.log.
 */
+function isTankEmpty(fuelLevel) {
+	if (fuelLevel === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(`Is tank empty? ${isTankEmpty(0)}`);
+console.log(`Is tank empty? ${isTankEmpty(45)}`);
