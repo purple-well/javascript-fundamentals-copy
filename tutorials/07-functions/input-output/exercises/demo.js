eba4f0e0cf6be750ca5e1b001e9e033454d51e1c
Chegg <your.email@example.com>
2026-05-21 17:05:54 -0500

    input and output demo juan m

--- tutorials/07-functions/input-output/exercises/demo.js
+++ tutorials/07-functions/input-output/exercises/demo.js
@@ -4,22 +4,47 @@
 
 // DEMO: Define a function to greet a crew member'crewName' is the parameter (the variable placeholder)
 
+function welcomeCrewMember(crewName) {
+    console.log(`Welcome back to the bridge, ${crewName}.`);
+}
+
 // DEMO: Invoke the function twice with two different names.
+welcomeCrewMember("Rabbit");
+welcomeCrewMember("Ozymandias");
+
 
 /** EXAMPLE 2 - MULTIPLE INPUTS **/
 
 // DEMO: Define a function that accepts distance and time. It should calculate velocity
 // and also print updates to the console.
+function calculateVelocity(distance, time) {
+    console.log(`Calculating speed for ${distance} meteres over ${time} seconds...`);
+    let speed = distance / time;
+    console.log(`Veolocity: ${speed} m/s`);
+}
+
 
 // DEMO: Call the function, passing in 100 meters and 10 seconds as arguments
+calculateVelocity(100, 10);
+
 
 /** EXAMPLE 3 - RETURN VALUES **/
 
 // DEMO: Define a function that accepts amounts of hydrogen and oxygen and returns the total
 // What happens if you code a message to print after the line with the return statement?
+function synthesizeWaterReactionMass(hydrogenTons, oxygenTons) {
+    let waterCompound = hydrogenTons + oxygenTons;
+    return waterCompound;
+
+    console.log("This text will not print. This code is just for fun!")
+}
+
 
 // DEMO: Call the function and pass in 50t of hydrogen and 25t of oxygen 
 // Store the result in a new variable and then print a sentence using it
+let currentReactionMass = synthesizeWaterReactionMass(50, 25);
+console.log(`Drive reserves updated: ${currentReactionMass} metric tons of H20 loaded.`);
+
 
 // DEMO: Make a git commit!
 
