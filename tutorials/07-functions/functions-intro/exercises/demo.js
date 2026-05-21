eacb52e5a8d5ba3a464f43fcf1140cab690094b7
Chegg <your.email@example.com>
2026-05-21 16:01:55 -0500

    functions demo juan m

--- tutorials/07-functions/functions-intro/exercises/demo.js
+++ tutorials/07-functions/functions-intro/exercises/demo.js
@@ -3,16 +3,27 @@
 /** EXAMPLE 1 - DECLARING & CALLING **/
 
 // DEMO: Declare a function that prints a report when solar panels are deployed
+function deploySolarPanels(){
+    console.log("Unlatching hinges...");
+    console.log("Solar panels fully deployed.");
+}
 
 // DEMO: Call the function twice in a row to see it being used and reused
+deploySolarPanels();
+deploySolarPanels();
 
 /** EXAMPLE 2 - CODE ORGANIZATION **/
 
 // DEMO: Declare a function that prints an emergency message
+function soundEmergencySiren() {
+    console.log("ALERT: HULL BREACH DETECTED!")
+}
+
 
 let hullBreach = true;
 
 if (hullBreach) {
+    soundEmergencySiren();
     // DEMO: Call the function to sound the siren
 }
 
